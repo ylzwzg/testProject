e867622428739b44955ba88b27a8839428db4e46
ylzwzg <wangzhigang@ylzfinancial.com>
2017-04-27 09:44:00 +0800

    te
    set

---
+++ test/test/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            decimal d = 0;
+            d = Math.Sign(1091 * 0.00016*100)/100;
+        }
+    }
+}
